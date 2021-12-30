67ba7fbb40c2d400ad5204942bf39937d8410fbc
Mike Anglebrandt <manglebrandt@epfc.com>
2021-12-30 13:02:06 -0500

    building the task form

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
-    public class CreateTaskModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class CreateTaskModel : PageModel
+	{
+		[BindProperty] public Task NewTask { get; set; }
+
+		public void OnGet()
+		{
+		}
+
+		public IActionResult OnPost()
+		{
+			return RedirectToPage("Index");
+		}
+	}
 }
